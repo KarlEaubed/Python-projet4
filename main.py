aa38efe78f034c3d38c901284ff494081d8143ff
LsGeeker <143838345+LsGeeker@users.noreply.github.com>
2023-11-13 22:56:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ if __name__ == "__main__" :
     input_file = 'bg.jpg'
     output_file = 'image.png'
     jpg_to_png(input_file, output_file)
+    input_file1 = 'background.png'
+    output_file1 = 'image1.jpg'
+    png_to_jpg(input_file1, outpout_file1)
